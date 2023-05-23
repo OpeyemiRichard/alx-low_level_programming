c40c99e30d29e3caf62cbd5f7735c3cf2e8cb056
Opeyemi Richard <opeyemirichard@gmail.com>
2023-05-22 22:20:56 -0700

    C program that prints exactly ...

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,8 @@
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stdout);
+	char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	fprintf(stderr, "%s", s);
 	return (1);
 }
